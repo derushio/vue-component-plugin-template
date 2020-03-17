468d7a7c7f4b2d429271f6e1ad039b6e4bfc2e7f
derushio <derushio@gmail.com>
2020-03-17 16:48:56 +0900

    <master>[new file]

--- src/utils/ArrayUtil.ts
+++ src/utils/ArrayUtil.ts
@@ -6,7 +6,7 @@ export default class ArrayUtil {
      * startからend(含む)までの配列を生成
      * startだけの場合はその数の配列を生成
      */
-    public static range(start: number, end?: number) {
+    public static range(start: number, end?: number): number[] {
         if (end == null) {
             end = start;
             start = 0;
@@ -17,5 +17,7 @@ export default class ArrayUtil {
         return [...Array(end).keys()].slice(start);
     }
 
-    protected constructor() {}
+    protected constructor() {
+        // pass;
+    }
 }
--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,13 +1,18 @@
 export enum LOG_LEVEL {
-    NONE, LOG, INFO, DEBUG, WARN, ERROR,
+    NONE,
+    LOG,
+    INFO,
+    DEBUG,
+    WARN,
+    ERROR,
 }
 
 export default class Logger {
     public history = [] as Array<{
-        time: string,
-        level: string,
-        message: any,
-        optionalParams: any[],
+        time: string;
+        level: string;
+        message: any;
+        optionalParams: any[];
     }>;
 
     protected logLevel = LOG_LEVEL.ERROR;
@@ -18,53 +23,58 @@ export default class Logger {
         this.historyNum = historyNum;
     }
 
-    public log(message?: any, ...optionalParams: any[]) {
+    public log(message?: any, ...optionalParams: any[]): void {
         if (this.logLevel >= LOG_LEVEL.LOG) {
-            window.console.log(message, ...optionalParams);
+            console.log(message, ...optionalParams);
             this.logger(LOG_LEVEL.LOG, message, optionalParams);
         }
     }
 
-    public info(message?: any, ...optionalParams: any[]) {
+    public info(message?: any, ...optionalParams: any[]): void {
         if (this.logLevel >= LOG_LEVEL.INFO) {
-            window.console.info(message, ...optionalParams);
+            console.info(message, ...optionalParams);
             this.logger(LOG_LEVEL.INFO, message, optionalParams);
         }
     }
 
-    public debug(message?: any, ...optionalParams: any[]) {
+    public debug(message?: any, ...optionalParams: any[]): void {
         if (this.logLevel >= LOG_LEVEL.DEBUG) {
-            window.console.debug(message, ...optionalParams);
+            console.debug(message, ...optionalParams);
             this.logger(LOG_LEVEL.DEBUG, message, optionalParams);
         }
     }
 
-    public table(...tabularData: any[]) {
+    public table(...tabularData: any[]): void {
         if (this.logLevel >= LOG_LEVEL.DEBUG) {
-            window.console.table(tabularData);
+            console.table(tabularData);
             this.logger(LOG_LEVEL.DEBUG, tabularData);
         }
     }
 
-    public warn(message?: any, ...optionalParams: any[]) {
+    public warn(message?: any, ...optionalParams: any[]): void {
         if (this.logLevel >= LOG_LEVEL.WARN) {
-            window.console.warn(message, ...optionalParams);
+            console.warn(message, ...optionalParams);
             this.logger(LOG_LEVEL.WARN, message, optionalParams);
         }
     }
 
-    public error(message?: any, ...optionalParams: any[]) {
+    public error(message?: any, ...optionalParams: any[]): void {
         if (this.logLevel >= LOG_LEVEL.ERROR) {
-            window.console.error(message, ...optionalParams);
+            console.error(message, ...optionalParams);
             this.logger(LOG_LEVEL.ERROR, message, optionalParams);
         }
     }
 
-    protected logger(level: LOG_LEVEL, message?: any, ...optionalParams: any[]) {
+    protected logger(
+        level: LOG_LEVEL,
+        message?: any,
+        ...optionalParams: any[]
+    ): void {
         this.history.unshift({
             time: new Date().toString(),
             level: LOG_LEVEL[level],
-            message, optionalParams,
+            message,
+            optionalParams,
         });
         this.history = this.history.slice(0, this.historyNum);
     }
